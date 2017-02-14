41392168ff9e66f58fa93fe6e4d7f9938d799a12
Kory Skarbek and Lawrence Eby <kory+lawrence@>
2017-02-14 13:56:26 -0800

    Fix bugs for returning all coins related to a value

--- Objects/Coins.cs
+++ Objects/Coins.cs
@@ -4,72 +4,77 @@ using System.IO;
 
 namespace CoinCombinationApp.Objects
 {
-  public class Coins
-  {
-    private int _userInput;
-    private int _penny;
-    private int _nickel;
-    private int _dime;
-    private int _quarter;
-    private static List<int> _changeList = new List<int>();
-
-    public Coins(int userInput)
+    public class Coins
     {
-      _userInput = userInput;
+        private int _userInput;
+        private int _penny;
+        private int _nickel;
+        private int _dime;
+        private int _quarter;
+        private static List<int> _changeList = new List<int>();
 
-      if(_userInput % 25 == 0)
-      {
-        _quarter = _userInput / 25;
-        _userInput -= _quarter * 25;
-        _changeList.Add(_quarter);
-      }
-      else if(_userInput % 10 == 0)
-      {
-        _dime = _userInput / 10;
-        _userInput -= _dime * 10;
-        _changeList.Add(_dime);
-      }
-      else if(_userInput % 5 == 0)
-      {
-        _nickel = _userInput / 5;
-        _userInput -= _nickel * 5;
-        _changeList.Add(_nickel);
-      }
-      else
-      {
-        _penny = _userInput;
-        _changeList.Add(_penny);
-      }
-    }
+        public Coins(int userInput)
+        {
+            _userInput = userInput;
+            Console.WriteLine("Before entering functions: " + _userInput);
+            if(_userInput % 25 >= 0)
+            {
+                Console.WriteLine("You're in quarters!");
+                _quarter = _userInput / 25;
+                _userInput = userInput % 25;
+                _changeList.Add(_quarter);
+            }
+            if(_userInput % 10 >= 0)
+            {
+                Console.WriteLine("You're in dimes!");
+                _dime = _userInput / 10;
+                _userInput = _userInput % 10;
+                _changeList.Add(_dime);
+            }
+            if(_userInput % 5 >= 0)
+            {
+                Console.WriteLine("You're in nickels!");
+                _nickel = _userInput / 5;
+                _userInput = userInput % 5;
+                _changeList.Add(_nickel);
+            }
+            if(_userInput >= 0)
+            {
+                Console.WriteLine("You're in pennies!");
+                _penny = _userInput;
+                _changeList.Add(_penny);
+            }
+            Console.WriteLine("After entering functions: " + _userInput);
+        }
 
-    public int GetPenny()
-    {
-      return _penny;
-    }
+        public int GetPenny()
+        {
+            return _penny;
+        }
 
-    public int GetNickel()
-    {
-      return _nickel;
-    }
+        public int GetNickel()
+        {
+            return _nickel;
+        }
 
-    public int GetDime()
-    {
-      return _dime;
-    }
+        public int GetDime()
+        {
+            return _dime;
+        }
 
-    public int GetQuarter()
-    {
-      return _quarter;
-    }
+        public int GetQuarter()
+        {
+            return _quarter;
+        }
 
-    public static List<int> GetAll()
-    {
-      return _changeList;
-    }
+        public static List<int> GetAll()
+        {
+            return _changeList;
+        }
 
-    public static void ClearAll()
-    {
-      _changeList.Clear();
+        public static void DeleteAll()
+        {
+            _changeList.Clear();
+        }
     }
-  }
 }
--- Tests/CoinTest.cs
+++ Tests/CoinTest.cs
@@ -6,70 +6,71 @@ using System.IO;
 
 namespace CoinTestApp
 {
-  public class CoinTestApp
-  {
-    [Fact]
-    public void IsCoin_ReturningPennies_Pennies()
+    public class CoinTestApp : IDisposable
     {
-      int change = 4;
+        [Fact]
+        public void IsCoin_ReturningPennies_Pennies()
+        {
+            int change = 4;
 
-      Coins newCoin = new Coins(change);
-      int result = newCoin.GetPenny();
-      Assert.Equal(change, result);
+            Coins newCoin = new Coins(change);
+            int result = newCoin.GetPenny();
+            Assert.Equal(change, result);
+        }
+
+        [Fact]
+        public void IsCoin_ReturningNickels_Nickels()
+        {
+            int change = 5;
+            int changeResult = change / 5;
+
+            Coins newCoin = new Coins(change);
+            int result = newCoin.GetNickel();
+            Assert.Equal(changeResult, result);
+        }
+
+        [Fact]
+        public void IsCoin_ReturningDimes_Dimes()
+        {
+            int change = 10;
+            int changeResult = change / 10;
+
+            Coins newCoin = new Coins(change);
+            int result = newCoin.GetDime();
+            Assert.Equal(changeResult, result);
+        }
+
+        [Fact]
+        public void IsCoin_ReturningQuarter_Quarter()
+        {
+            int change = 25;
+            int changeResult = change / 25;
+
+            Coins newCoin = new Coins(change);
+            int result = newCoin.GetQuarter();
+            Assert.Equal(changeResult, result);
+        }
+
+        [Fact]
+        public void IsCoin_ReturningAll_All()
+        {
+            int change = 100;
+            List<int> changeResult = new List<int>();
+            changeResult.Add(4);
+            changeResult.Add(0);
+            changeResult.Add(0);
+            changeResult.Add(0);
+
+
+            Coins newCoin = new Coins(change);
+            List<int> result = Coins.GetAll();
+
+            Assert.Equal(changeResult, result);
+        }
+
+        public void Dispose()
+        {
+          Coins.DeleteAll();
+        }
     }
-
-    [Fact]
-    public void IsCoin_ReturningNickels_Nickels()
-    {
-      int change = 5;
-      int changeResult = change / 5;
-
-      Coins newCoin = new Coins(change);
-      int result = newCoin.GetNickel();
-      Assert.Equal(changeResult, result);
-      Coins.ClearAll();
-    }
-
-    [Fact]
-    public void IsCoin_ReturningDimes_Dimes()
-    {
-      int change = 10;
-      int changeResult = change / 10;
-
-      Coins newCoin = new Coins(change);
-      int result = newCoin.GetDime();
-      Assert.Equal(changeResult, result);
-      Coins.ClearAll();
-    }
-
-    [Fact]
-    public void IsCoin_ReturningQuarter_Quarter()
-    {
-      int change = 25;
-      int changeResult = change / 25;
-
-      Coins newCoin = new Coins(change);
-      int result = newCoin.GetQuarter();
-      Assert.Equal(changeResult, result);
-      Coins.ClearAll();
-    }
-
-    [Fact]
-    public void IsCoin_ReturningAll_All()
-    {
-      int change = 41;
-      List<int> changeResult = new List<int>();
-      changeResult.Add(1);
-      changeResult.Add(1);
-      changeResult.Add(1);
-      changeResult.Add(1);
-
-
-      Coins newCoin = new Coins(change);
-      List<int> result = Coins.GetAll();
-
-      Assert.Equal(changeResult, result);
-      Coins.ClearAll();
-    }
-  }
 }
